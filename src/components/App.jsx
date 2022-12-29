fe305eec0ad7da6db5706e99df670972d8acb91d
Tatyana Syrbu <tanja.karamilja@gmail.com>
2022-12-29 23:05:38 +0200

    summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,61 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, Suspense, lazy } from 'react';
+import operations from 'Redux/auth/authOperations';
+import authSelectors from 'Redux/auth/authSelectors';
+
+import AppBar from './AppBar';
+import { Loader } from './Loader/Loader';
+import PrivateRoute from './Routes/PrivateRoute';
+import PublicRoute from './Routes/PublicRoute';
+
+const LoginView = lazy(() =>
+  import('pages/LoginView/LoginView')
+);
+const RegisterView = lazy(() =>
+  import(
+    'pages/RegisterView/RegisterView')
+);
+const ContactsView = lazy(() =>
+  import(
+    'pages/ContactsView/ContactsView' /* webpackChunkName: "contacts-view" */
+  )
+);
+
+function App() {
+  const dispatch = useDispatch();
+  const isFetchCurrentUser = useSelector(authSelectors.getIsFetchCurrentUser);
+  console.log(isFetchCurrentUser);
+
+  useEffect(() => {
+    dispatch(operations.fetchCurrentUser());
+  }, [dispatch]);
+  return isFetchCurrentUser ? (
+    <Loader />
+  ) : (
+    <div>
+      <AppBar />
+      <Suspense fallback={<h1>Loading...</h1>}>
+        <Routes>
+          <Route element={<PublicRoute restricted redirectTo="contacts" />}>
+            <Route path="/" element={<LoginView />} />
+          </Route>
+          <Route element={<PublicRoute restricted redirectTo="contacts" />}>
+            <Route path="register" element={<RegisterView />} />
+          </Route>
+          <Route element={<PublicRoute restricted redirectTo="contacts" />}>
+            <Route path="login" element={<LoginView />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="contacts" element={<ContactsView />} />
+          </Route>
+          <Route element={<PublicRoute restricted redirectTo="contacts" />}>
+            <Route path="*" element={<Navigate to="login" />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </div>
   );
-};
+}
+
+export default App;
